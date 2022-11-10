be7ace1266ea5eb63adb9684e87455573ec1a632
Soumya Sharma <soumy99sharma@gmail.com>
2022-11-11 00:40:30 +0530

    Added difficulty levels

--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,55 @@
 import React, { useEffect, useState } from "react";
 import MineSweeper from "./minesweeper";
-import './App.css';
+import "./App.css";
+import Grid from "./Grid";
+
+interface DifficultyLevel {
+  levelName: string;
+  height: number;
+  width: number;
+  mines: number;
+}
+
+const difficultyLevels: DifficultyLevel[] = [
+  {
+    levelName: "easy",
+    height: 7,
+    width: 7,
+    mines: 10,
+  },
+  {
+    levelName: "medium",
+    height: 10,
+    width: 10,
+    mines: 30,
+  },
+  {
+    levelName: "hard",
+    height: 15,
+    width: 15,
+    mines: 70,
+  },
+];
 
 function App() {
-  const ms = new MineSweeper(5, 5, 3);
-  const [grid, setGrid] = useState<number[][]>(ms.grid);
-  const [isGameOver, setIsGameOver] = useState<boolean>(false);
-  const [isGameWon, setIsGameWon] = useState<boolean>(false);
-  const [clicks, setClick] = useState<number>(0);
-  const [cellsLeft, setCellsLeft] = useState(22);
-  function visitNeighbors(
-    row: number,
-    col: number,
-    grid: number[][]
-    //cellsLeft: number
-  ): void {
-    // visit all its neighbours
-    const neighbours: number[][] = [
-      [-1, -1],
-      [-1, 0],
-      [-1, 1],
-      [0, -1],
-      [0, 1],
-      [1, -1],
-      [1, 0],
-      [1, 1],
-    ];
-    let nearbyMines: number = 0;
-    const safeNb: number[][] = [];
-    for (const nb of neighbours) {
-      const nextRow = row + nb[0];
-      const nextCol = col + nb[1];
-      if (
-        nextRow < 0 ||
-        nextRow >= grid.length ||
-        nextCol < 0 ||
-        nextCol >= grid[0].length
-      )
-        continue;
-      if (grid[nextRow][nextCol] === Number.POSITIVE_INFINITY) {
-        nearbyMines++;
-      } else if (grid[nextRow][nextCol] === 0) {
-        const newCell = [nextRow, nextCol];
-        safeNb.push(newCell);
-      }
-    }
-    if (nearbyMines === 0) {
-      // no mines in its neighbours
-      grid[row][col] = Number.NEGATIVE_INFINITY;
-      setCellsLeft((cellsLeft) => --cellsLeft);
-      // go to its neighbours
-      for (const nb of safeNb) {
-        if (grid[nb[0]][nb[1]] === 0) visitNeighbors(nb[0], nb[1], grid);
-      }
-    } else {
-      grid[row][col] = nearbyMines;
-      setCellsLeft((cellsLeft) => --cellsLeft);
-    }
-  }
-  const cellClickHandler = (event: React.MouseEvent<HTMLElement>) => {
-    if (isGameOver || isGameWon) return;
-    const target: HTMLElement = event.currentTarget;
-    const r = parseInt(target.getAttribute("data-row") || "");
-    const c = parseInt(target.getAttribute("data-col") || "");
-    if (grid[r][c] === Number.POSITIVE_INFINITY) {
-      setIsGameOver(true);
-    } else if (grid[r][c] === 0) {
-      setClick((click) => ++click);
-      setGrid((grid) => {
-        visitNeighbors(r, c, grid);
-        return [...grid];
-      });
-    }
+  const [difficulty, setDifficulty] = useState<number>(0);
+	//const [height,setHeight]=useState<number>(0);
+	//const [width,setwidth]=useState<number>(0);
+	//const [mines,setmines]=useState<number>(0);
+  const difficultyChangeHandler = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setDifficulty(parseInt(event.target.value));
   };
-  useEffect(() => {
-    if (cellsLeft === 0) setIsGameWon(true);
-  }, [cellsLeft]);
   return (
     <div className="App">
-      <p>Clicks: {clicks}</p>
-      <p>Cells Left: {cellsLeft}</p>
-      {isGameOver ? <p>GAME OVER!!!!💣</p> : null}
-      {isGameWon ? <p>🎉You won 🎉</p> : null}
-      <div className="grid">
-        {grid.map((row, r) => {
-          return (
-            <div className="row" key={`row-${r}`}>
-              {row.map((col, c) => {
-                return (
-                  <button
-									className="cell"
-                    key={`col-${c}`}
-                    data-row={r}
-                    data-col={c}
-                    onClick={cellClickHandler}
-                  >
-                    {col === Number.NEGATIVE_INFINITY
-                      ? "_ "
-                      : col === Number.POSITIVE_INFINITY
-                      ? isGameWon || isGameOver
-                        ? "💣"
-                        : "0 "
-                      : col}
-                  </button>
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
+      <label>Difficulty</label>
+      <select value={difficulty} onChange={difficultyChangeHandler}>
+        <option value={0}>Easy</option>
+        <option value={1}>Medium</option>
+        <option value={2}>Hard</option>
+      </select>
+      <Grid {...difficultyLevels[difficulty]} />
     </div>
   );
 }
---
+++ src/Grid.tsx
@@ -0,0 +1,138 @@
+import React, { useEffect, useState } from "react";
+import MineSweeper from "./minesweeper";
+import "./App.css";
+
+const Grid = (props: { height: number; width: number; mines: number;levelName:string }) => {
+  const { height, width, mines,levelName } = props;
+	console.log({props})
+  const [grid, setGrid] = useState<number[][]>([]);
+	console.log({grid});
+  const [isGameOver, setIsGameOver] = useState<boolean>(false);
+  const [isGameWon, setIsGameWon] = useState<boolean>(false);
+  const [clicks, setClick] = useState<number>(0);
+  const [cellsLeft, setCellsLeft] = useState(width*height-mines);
+  function visitNeighbors(
+    row: number,
+    col: number,
+    grid: number[][]
+    //cellsLeft: number
+  ): void {
+    // visit all its neighbours
+    const neighbours: number[][] = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
+    ];
+    let nearbyMines: number = 0;
+    const safeNb: number[][] = [];
+    for (const nb of neighbours) {
+      const nextRow = row + nb[0];
+      const nextCol = col + nb[1];
+      if (
+        nextRow < 0 ||
+        nextRow >= grid.length ||
+        nextCol < 0 ||
+        nextCol >= grid[0].length
+      )
+        continue;
+      if (grid[nextRow][nextCol] === Number.POSITIVE_INFINITY) {
+        nearbyMines++;
+      } else if (grid[nextRow][nextCol] === 0) {
+        const newCell = [nextRow, nextCol];
+        safeNb.push(newCell);
+      }
+    }
+    if (nearbyMines === 0) {
+      // no mines in its neighbours
+      grid[row][col] = Number.NEGATIVE_INFINITY;
+      setCellsLeft((cellsLeft) => --cellsLeft);
+      // go to its neighbours
+      for (const nb of safeNb) {
+        if (grid[nb[0]][nb[1]] === 0) visitNeighbors(nb[0], nb[1], grid);
+      }
+    } else {
+      grid[row][col] = nearbyMines;
+      setCellsLeft((cellsLeft) => --cellsLeft);
+    }
+  }
+	useEffect(()=>{
+		const ms = new MineSweeper(height, width, mines);
+		setGrid(ms.grid);
+	},[height, mines, width]);
+  const cellClickHandler = (event: React.MouseEvent<HTMLElement>) => {
+    if (isGameOver || isGameWon) return;
+    const target: HTMLElement = event.currentTarget;
+    const r = parseInt(target.getAttribute("data-row") || "");
+    const c = parseInt(target.getAttribute("data-col") || "");
+    if (grid[r][c] === Number.POSITIVE_INFINITY) {
+      setIsGameOver(true);
+    } else if (grid[r][c] === 0) {
+      setClick((click) => ++click);
+      setGrid((grid) => {
+        visitNeighbors(r, c, grid);
+        return [...grid];
+      });
+    }
+  };
+  useEffect(() => {
+    if (cellsLeft === 0) {
+      setIsGameWon(true);
+      setIsGameOver(true);
+    }
+  }, [cellsLeft]);
+  return (
+    <div className="Grid">
+			<h1>Level: {levelName}</h1>
+      <p>Clicks: {clicks}</p>
+      <p>Cells Left: {cellsLeft}</p>
+      {isGameOver ? (
+        isGameWon ? (
+          <p>🎉You won 🎉</p>
+        ) : (
+          <p>GAME OVER!!!!💣</p>
+        )
+      ) : null}
+      <div className="grid">
+        {grid.map((row, r) => {
+          return (
+            <div className="row" key={`row-${r}`}>
+              {row.map((col, c) => {
+                return (
+                  <button
+                    className={`cell ${
+                      isGameOver
+                        ? "uncovered"
+                        : col === Number.NEGATIVE_INFINITY ||
+                          (col > 0 && col !== Number.POSITIVE_INFINITY)
+                        ? "uncovered"
+                        : "covered"
+                    }`}
+                    key={`col-${c}`}
+                    data-row={r}
+                    data-col={c}
+                    onClick={cellClickHandler}
+                  >
+                    {col === Number.NEGATIVE_INFINITY || col === 0
+                      ? " "
+                      : col === Number.POSITIVE_INFINITY
+                      ? isGameOver
+                        ? "💣"
+                        : " "
+                      : col}
+                  </button>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Grid;
